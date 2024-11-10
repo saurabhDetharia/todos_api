62c749be6dbb6774d047ede2befc4545e77805af
saurabhdetharia <dethariasaurabh7@gmail.com>
2024-11-11 01:00:40 +0530

    updated engine

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+        # 'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 }
 
